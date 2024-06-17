43820fe7a9605be3774c9fdcf38e861a08a63ab9
cheived <cheived55@gmail.com>
2024-06-17 22:25:33 +0400

    CORS setup

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from crud import (
     get_post_by_id,
     update_existing_post,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,6 +25,14 @@ def get_db():
 
 
 app = FastAPI(title="Ales backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/posts", response_model=PostsRead)
